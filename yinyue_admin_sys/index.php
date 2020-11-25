90c198b8c1aabf4c2f7bb98b2d54191a69dcc998
jiayi-99 <73356246+jiayi-99@users.noreply.github.com>
2020-11-24 23:35:21 -0800

    Update index.php

--- yinyue_admin_sys/index.php
+++ yinyue_admin_sys/index.php
@@ -9,9 +9,9 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-// 应用入口文件
+// Application entry file
 
-// 检测PHP环境
+// detect PHP environment
 if (version_compare(PHP_VERSION, '5.3.0', '<')) {
     die('require PHP > 5.3.0 !');
 }
@@ -21,16 +21,16 @@ header('Access-Control-Allow-Methods:POST,GET,OPTIONS,DELETE');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: Content-Type, X-Requested-With,token,Accept,Authorization,Origin');
 
-// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+// Turn on the debugging mode. It is recommended to turn on the development stage. The deployment stage comment or set to false
 define('APP_DEBUG', true);
 
-// 定义应用目录
+// Define application directory
 define('APP_PATH', './Application/');
 
-// 绑定模块到当前入口文件
+// Bind the module to the current entry file
 define('BIND_MODULE', 'Index');
 
-// 引入ThinkPHP入口文件
+// Import the ThinkPHP entry file
 require './ThinkPHP/ThinkPHP.php';
 
-// 亲^_^ 后面不需要任何代码了 就是如此简单
+
