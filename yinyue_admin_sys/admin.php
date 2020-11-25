0747b5d25e30ec917d253c076e9bbbadd251a673
jiayi-99 <73356246+jiayi-99@users.noreply.github.com>
2020-11-24 23:36:36 -0800

    Update admin.php

--- yinyue_admin_sys/admin.php
+++ yinyue_admin_sys/admin.php
@@ -9,9 +9,7 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-// 应用入口文件
 
-// 检测PHP环境
 if (version_compare(PHP_VERSION, '5.3.0', '<')) {
     die('require PHP > 5.3.0 !');
 }
@@ -20,16 +18,16 @@ if (is_file(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
 
-// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+
 define('APP_DEBUG', true);
 
-// 定义应用目录
+
 define('APP_PATH', './Application/');
 
-// 绑定模块到当前入口文件
+
 define('BIND_MODULE', 'Admin');
 
-// 引入ThinkPHP入口文件
+
 require './ThinkPHP/ThinkPHP.php';
 
-// 亲^_^ 后面不需要任何代码了 就是如此简单
+
